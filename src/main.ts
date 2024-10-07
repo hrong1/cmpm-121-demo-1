7fb636fe122a83f9dc6ab7664a0190c3761c744c
hrong1 <hrong1@ucsc.edu>
2024-10-06 23:08:25 -0700

    Step 1 button add

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,16 @@ const app: HTMLDivElement = document.querySelector("#app")!;
 const gameName = "A Clicking Game";
 document.title = gameName;
 
-const header = document.createElement("h1");
-header.innerHTML = gameName;
-app.append(header);
+const buttonemoji = "💡";
+
+
+const button = document.createElement("button");
+button.innerHTML = buttonemoji;
+
+
+
+app.append(button)
+
+// const header = document.createElement("h1");
+// header.innerHTML = gameName;
+// app.append(header);
