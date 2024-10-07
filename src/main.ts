a0f7064c8a44ea1fa6d7343ed2126e8d583a8ba0
hrong1 <hrong1@ucsc.edu>
2024-10-06 23:27:10 -0700

    step 2 bug fixed

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import "./style.css";
 const app: HTMLDivElement = document.querySelector("#app")!;
 const number_count: HTMLDivElement = document.querySelector("#number_count")!;
 
-
 const gameName = "A Clicking Game";
 document.title = gameName;
 
@@ -11,17 +10,18 @@ const buttonemoji = "💡";
 let counter: number = 0;
 
 const button = document.createElement("button");
+const number_display = document.createElement("number_display");
 button.innerHTML = buttonemoji;
-number_count.innerText = `Number of Item: ${counter}`;
+number_display.innerText = `Number of Item: ${counter}`;
 
-button.addEventListener('click', () => {
-    counter += 1;
-    console.log(counter);
-    number_count.innerText = `Number of Item: ${counter}`;
-  });
+button.addEventListener("click", () => {
+  counter += 1;
+  console.log(counter);
+  number_display.innerText = `Number of Item: ${counter}`;
+});
 
 app.append(button);
-number_count.append(number_count);
+number_count.append(number_display);
 
 // const header = document.createElement("h1");
 // header.innerHTML = gameName;
