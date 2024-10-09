77724cf6713d9b07465015e5411190b7364e38f2
hrong1 <hrong1@ucsc.edu>
2024-10-08 23:19:03 -0700

    step 4 complete

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,47 @@
-
 import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
 const gameName = "A Clicking Game";
 document.title = gameName;
+const header = document.createElement("h1");
+header.innerHTML = gameName;
+app.append(header);
 
-const buttonemoji = "💡";
+const buttonemoji = "💰";
 let counter: number = 0;
+let auto_increase_number: number = 1;
 
 const button = document.createElement("button");
 const number_display = document.createElement("number_display");
 button.innerHTML = buttonemoji;
-number_display.innerText = `Number of Item: ${counter}`;
+number_display.innerHTML = `Money: ${counter}`;
 
 button.addEventListener("click", () => {
   counter += 1;
-  console.log(counter);
-  number_display.innerText = `Number of Item: ${counter}`;
 });
+app.append(button);
 
 function Autoclick() {
-  counter += 1;
-  number_display.innerText = `Number of Item: ${counter}`;
+  counter += auto_increase_number;
 }
 setInterval(Autoclick, 1000);
 
-app.append(button);
+
+let lastTime = 0;
+let fps = 0;
+function animate(timestamp: number) {
+  const now = timestamp;
+  const deltaTime = now - lastTime;
+  lastTime = now;
+  fps = 1000 / deltaTime;
+  // update counter
+  counter = Number(counter.toFixed(2));
+  number_display.innerText = `Money: ${counter}`;
+  requestAnimationFrame(animate);
+}
+requestAnimationFrame(animate);
+
 app.append(number_display);
 
-// const header = document.createElement("h1");
-// header.innerHTML = gameName;
-// app.append(header);
+
