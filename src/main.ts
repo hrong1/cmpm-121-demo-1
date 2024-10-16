6e560b68e42a066ad398d29406bb881230e2aed4
hrong1 <hrong1@ucsc.edu>
2024-10-15 22:32:28 -0700

    step 2 completed

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,24 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
+
+
 const gameName = "A Clicking Game";
 document.title = gameName;
 
 const buttonemoji = "💡";
+let counter: number = 0;
 
 const button = document.createElement("button");
+const number_count = document.createElement("div");
 button.innerHTML = buttonemoji;
+number_count.innerText = `Number of Item: ${counter}`;
 
-app.append(button)
+button.addEventListener('click', () => {
+    counter += 1;
+    console.log(counter);
+    number_count.innerText = `Number of Item: ${counter}`;
+  });
+
+app.append(button);
+app.append(number_count);
