3a32fb4e7ec84ea302999054aba03538d0bc1a6d
hrong1 <hrong1@ucsc.edu>
2024-10-15 22:47:20 -0700

    step 5 completed

--- src/main.ts
+++ src/main.ts
@@ -10,10 +10,13 @@ app.append(header);
 
 const buttonemoji = "💰";
 let counter: number = 0;
-const auto_increase_number: number = 1;
+let auto_increase_number: number = 0;
+let upgrade_cost:number = 10;
 
 const button = document.createElement("button");
 const number_display = document.createElement("div");
+const increase_number = document.createElement("div");
+const upgrade = document.createElement("button");
 button.innerHTML = buttonemoji;
 number_display.innerHTML = `Money: ${counter}`;
 
@@ -22,17 +25,30 @@ button.addEventListener("click", () => {
 });
 app.append(button);
 
+upgrade.addEventListener("click", () => {
+  auto_increase_number += 1;
+  counter -= upgrade_cost
+  upgrade_cost *= 1.1;
+});
+app.append(button);
 
 
 let lastTime = performance.now();
 requestAnimationFrame(function increaseCounter() {
+  if (counter < upgrade_cost) upgrade.disabled = true;
+  else upgrade.disabled = false;
   const currentTime = performance.now();
   const deltaTime = currentTime - lastTime;
   counter += (deltaTime / 1000) * auto_increase_number;
   number_display.innerHTML =
     `${(Math.floor(counter * 100) / 100).toString()}` + " dollars";
+  upgrade_cost = Number(upgrade_cost.toFixed(2));
+  upgrade.innerHTML = "Upgrade" + `(Cost: ${upgrade_cost})`;
+  increase_number.innerText = `Auto Increase ${auto_increase_number} per second`
   lastTime = currentTime;
   requestAnimationFrame(increaseCounter);
 });
 
 app.append(number_display);
+app.append(increase_number);
+app.append(upgrade);
